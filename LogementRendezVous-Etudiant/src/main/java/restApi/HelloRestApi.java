f073d377e36859e1513b7a06adc31d2833f41351
AhmedHICHRI1996 <ahmed.hichri1000@gmail.com>
2025-05-22 20:20:04 +0100

    Atelier SOA first commit ahmed on main toul

---
+++ LogementRendezVous-Etudiant/src/main/java/restApi/HelloRestApi.java
@@ -0,0 +1,16 @@
+package restApi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+@Path("/hi")
+public class HelloRestApi {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/hello")
+    public javax.ws.rs.core.Response sayHello(){
+        return Response.status(200).entity("hello world!").build();
+    }
+}
