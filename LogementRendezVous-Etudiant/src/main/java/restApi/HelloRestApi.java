e9ded7f817c544086e7633d52759c3abab9b898a
Ensaff <insaf.eleuchi@esprit.tn>
2025-05-22 10:27:30 +0100

    restApi sayHello

---
+++ LogementRendezVous-Etudiant/src/main/java/restApi/HelloRestApi.java
@@ -0,0 +1,22 @@
+package restApi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+//exposition du web service
+//ressource = web service = endpoint =restapi
+@Path("/hello")  //specifier la route vers la ressource
+public class HelloRestApi {
+    //web service --> return hello to the client
+    //response --> http REQUEST / Header + Body
+    @GET //pr dire c est une ressources a consommer à travaers une requette http de type get
+    @Produces (MediaType.TEXT_PLAIN)  //pr specifier le type de retour text, json,xml,html
+    public Response sayHello() {
+        return Response
+                .status(200)
+                .entity("Hello World!")
+                .build();
+    }
+}
