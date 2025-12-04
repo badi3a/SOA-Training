4b5cc22b0b10ba153e0eddb6831c21ac6f298441
marwa-chaibi <Chaibi.MARWA@esprit.com>
2025-12-04 10:04:51 +0100

    Ajout projet Gestion_UE_Etudiant

---
+++ Gestion_UE_Etudiant/src/main/java/webservices/ModuleRessources.java
@@ -0,0 +1,27 @@
+package webservices;
+
+// Import the necessary JAX-RS (Java API for RESTful Web Services) annotations and classes
+import javax.ws.rs.GET;               // Annotation to indicate that a method responds to an HTTP GET request
+import javax.ws.rs.Path;              // Annotation to define the URL path of a resource
+import javax.ws.rs.Produces;          // Annotation to define the type of content returned (text, JSON, XML…)
+import javax.ws.rs.core.MediaType;    // Provides constants for standard media types (e.g., TEXT_PLAIN, APPLICATION_JSON…)
+import javax.ws.rs.core.Response;     // Class used to build a custom HTTP response
+
+import static webservices.UERessources.helper;
+
+// The resource will be accessible at the base URL followed by /hello
+@Path("/hello")
+public class ModuleRessources {
+
+    // This method handles GET requests sent to /hello/hi
+    @GET
+    @Path("/hi")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllUEs() {
+        return Response
+                .status(200)
+                .entity(helper.getListeUE())
+                .build();
+    }
+
+}
---
+++ Gestion_UE_Etudiant/src/main/java/webservices/UERessources.java
@@ -0,0 +1,28 @@
+package webservices;
+
+import metiers.UniteEnseignementBusiness;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/ue")
+public class UERessources
+{
+    static UniteEnseignementBusiness helper = new UniteEnseignementBusiness();
+    @Path("/list")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllUEs()
+    {
+        return Response
+                .status(200)
+                .entity(helper.getListeUE())
+                .build();
+
+    }
+
+    }
+
